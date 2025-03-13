b3f336c3bcce1f54a862bc7a8cd3441db564c7c1
fizziedrinks <ivele3@illinois.edu>
2025-03-12 23:21:08 -0500

    Added Product, Cart, and About page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Home from "./pages/Home";
+import Product from "./pages/Product";
+import Cart from "./pages/Cart";
+import About from "./pages/About";
 
 function App() {
   return (
     <Router>
+      <nav className="p-4 bg-gray-200">
+        <ul className="flex space-x-4">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/browse">Product</Link></li>
+          <li><Link to="/cart">Cart</Link></li>
+          <li><Link to="/about">About</Link></li>
+        </ul>
+      </nav>
+
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/browse" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </Router>
   );
